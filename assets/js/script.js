623f32081c5ae71f3c2fd8fa2d7169cb0655da2d
Liam Snowdon <snowdon.liam@gmail.com>
2018-08-15 11:02:00 +0100

    Add contact form and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,30 @@ swiper.el.addEventListener("mouseenter", function(event) {
 swiper.el.addEventListener("mouseleave", function(event) {
     swiper.autoplay.start();
 }, false);
+
+// Contact form
+var ContactForm = {
+    fields: document.querySelectorAll('.js-field'),
+    addErrorEvents: function() {
+        for (var i = 0; i < this.fields.length; i++) {
+
+            this.fields[i].addEventListener('invalid', function(event) {
+                event.preventDefault();
+
+                var errorMessage = this.nextElementSibling;
+                errorMessage.style.display = 'block';
+                errorMessage.innerText = 'This field is invalid.'
+            });
+
+            this.fields[i].addEventListener('input', function() {
+                var errorMessage = this.nextElementSibling;
+                if ('block' === errorMessage.style.display) {
+                    errorMessage.style.display = 'none';
+                }
+            });
+        }
+    }
+};
+
+ContactForm.addErrorEvents();
+
